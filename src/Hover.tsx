4deb277928671979d897ca820c015e4c12b30847
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-04-18 18:44:31 +0200

    Start to implement hover effects

--- src/Hover.tsx
+++ src/Hover.tsx
@@ -1,31 +1,28 @@
-interface Prop {
-  cursorPosition: {
-    x: number;
-    y: number;
-  };
+export interface ICursorPosition {
+  x: number;
+  y: number;
 }
 
-export default function Hover({ cursorPosition }: Prop) {
+export default function Hover(cursorPosition: ICursorPosition) {
   return (
     <>
-      {cursorPosition.x !== -1 && cursorPosition.y !== -1 && (
-        <div
-          className="circle"
-          style={{
-            position: "absolute",
-            top: cursorPosition.y,
-            left: cursorPosition.x,
-            width: "48vw",
-            transform: "translate(-25%) ",
-            height: "42vw",
-            borderRadius: "50%",
-            filter: "blur(60px) drop-shadow(3px 3px red) hue-rotate(90deg) ",
-            opacity: 0.2,
-            cursor: "none",
-            backgroundColor: "pink",
-          }}
-        />
-      )}
+      <div
+        className="circle"
+        style={{
+          position: "absolute",
+          top: cursorPosition.y,
+          left: cursorPosition.x,
+          width: "48vw",
+          transform: "translate(-25%) ",
+          height: "42vw",
+          borderRadius: "50%",
+          filter: "blur(60px) drop-shadow(3px 3px red) hue-rotate(90deg) ",
+          opacity: 0.2,
+          cursor: "none",
+          zIndex: 89,
+          backgroundColor: "pink",
+        }}
+      />
     </>
   );
 }
