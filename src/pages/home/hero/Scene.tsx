96005c85c75db4f3fb25103a5a3f2f552bc4b3ff
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-04-18 18:45:30 +0200

    Scene with spinning circles, wip

--- src/pages/home/hero/Scene.tsx
+++ src/pages/home/hero/Scene.tsx
@@ -9,17 +9,19 @@ export default function Scene() {
 
   useEffect(() => {
     const scene = new THREE.Scene();
-    const geometry = new THREE.SphereGeometry(3, 64, 64);
-
-    const circles = [];
-    const numCircles = 5;
+    let geometry;
+    if (innerWidth > 600) {
+      geometry = new THREE.SphereGeometry(3, 64, 64);
+    } else {
+      geometry = new THREE.SphereGeometry(1, 21, 21);
+    }
 
     const materialLit = new THREE.MeshPhysicalMaterial({
       color: isDarkmode ? 0xffffff : "skyblue",
       roughness: 0.01,
       metalness: 0.9,
       transparent: true,
-      opacity: 0.5,
+      opacity: 0.2,
     });
 
     const meshLit = new THREE.Mesh(geometry, materialLit);
@@ -30,19 +32,19 @@ export default function Scene() {
       roughness: 2,
       metalness: 0.9,
       transparent: true,
-      opacity: isDarkmode ? 0.2 : 0.8,
+      opacity: isDarkmode ? 0.2 : 0.2,
     });
 
     const meshLight = new THREE.Mesh(geometry, materialShadow);
     scene.add(meshLight);
 
-    const light = new THREE.PointLight(0xffffff, 173, 100);
-    scene.add(light);
-    light.position.set(6, 4, 10);
+    const smallLight = new THREE.PointLight(0xffffff, 200, 100);
+    smallLight.position.set(6, 10, 1);
+    scene.add(smallLight);
 
     const sizes = {
       width: window.innerWidth,
-      height: window.innerWidth,
+      height: window.innerHeight,
     };
 
     const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height);
@@ -55,20 +57,23 @@ export default function Scene() {
     renderer.setClearColor(0x000000, 0);
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(2);
+    renderer.render(scene, camera);
 
     materialLit.roughness = 0;
     materialLit.transparent = true;
     materialLit.opacity = 0.2;
     materialLit.alphaMap;
+    materialLit.envMapIntensity = 1.2;
+    materialLit.transmission = 1;
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
     controls.enableZoom = false;
     controls.enableRotate = true;
     controls.autoRotate = true;
-    controls.autoRotateSpeed = 3;
+    controls.autoRotateSpeed = 2;
 
-    renderer.render(scene, camera);
+    const numCircles = 5;
 
     for (let i = 0; i < numCircles; i++) {
       const geometry = new THREE.SphereGeometry(1, 32, 32);
@@ -80,21 +85,23 @@ export default function Scene() {
       mesh.position.z = Math.random() * 20 - 10;
 
       scene.add(mesh);
-      circles.push(mesh);
     }
 
     const handleResize = () => {
+      // update sizes
       sizes.width = window.innerWidth;
       sizes.height = window.innerHeight;
+
+      // update camera
       camera.aspect = sizes.width / sizes.height;
-      camera.updateProjectionMatrix();
       renderer.setSize(sizes.width, sizes.height);
+      camera.updateProjectionMatrix();
     };
 
     const animate = () => {
+      renderer.render(scene, camera);
       requestAnimationFrame(animate);
       controls.update();
-      renderer.render(scene, camera);
     };
 
     animate();
