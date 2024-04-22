5f077194e5f517deae7f43c94d88b703390e7aa3
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2024-04-22 20:32:53 +0200

    Update tech component + style fixes

--- src/pages/home/tech/Tech.tsx
+++ src/pages/home/tech/Tech.tsx
@@ -1,31 +1,50 @@
+import { useTranslation } from "../../../context/TranslationContext";
+
 export default function Tech() {
+  const { activeLang } = useTranslation();
+
   return (
     <div className="programming">
       <div className="programming__inner">
-        <h4 className="hidden programming__title">
-          Programmering & designverktyg
-        </h4>
+        <h3 className="hidden programming__title" tabIndex={0}>
+          {activeLang === "sv" ? translations.sv.title : translations.en.title}
+        </h3>
         <div className="hidden programming__columns">
           <div className="hidden programming__column-1 programming__column">
-            <h6 className="hidden programming__item">HTML</h6>
-            <h6 className="hidden programming__item">CSS</h6>
-            <h6 className="hidden programming__item">Sass</h6>
-            <h6 className="hidden programming__item">Javascript</h6>
+            <p className="hidden programming__item">HTML</p>
+            <p className="hidden programming__item">CSS</p>
+            <p className="hidden programming__item">Sass/SCSS</p>
+            <p className="hidden programming__item">Emotion</p>
           </div>
           <div className="hidden programming__column-2 programming__column">
-            <h6 className="hidden programming__item">Typescript</h6>
-            <h6 className="hidden programming__item">React</h6>
-            <h6 className="hidden programming__item">Wordpress</h6>
-            <h6 className="hidden programming__item">PHP</h6>
+            <p className="hidden programming__item">Javascript</p>
+            <p className="hidden programming__item">Typescript</p>
+            <p className="hidden programming__item">React</p>
+            <p className="hidden programming__item">Wordpress</p>
+          </div>
+          <div className="hidden programming__column-2 programming__column">
+            <p className="hidden programming__item">Node.js</p>
+            <p className="hidden programming__item">Next.js</p>
+            <p className="hidden programming__item">Mongo DB</p>
+            <p className="hidden programming__item">Mongoose</p>
           </div>
           <div className="hidden programming__column-3 programming__column">
-            <h6 className="hidden programming__item">Illustrator</h6>
-            <h6 className="hidden programming__item">XD</h6>
-            <h6 className="hidden programming__item">Figma</h6>
-            <h6 className="hidden programming__item">Photoshop</h6>
+            <p className="hidden programming__item">Illustrator</p>
+            <p className="hidden programming__item">XD</p>
+            <p className="hidden programming__item">Figma</p>
+            <p className="hidden programming__item">Photoshop</p>
           </div>
         </div>
       </div>
     </div>
   );
 }
+
+const translations = {
+  sv: {
+    title: "Programmering & designverktyg",
+  },
+  en: {
+    title: "Programing & design tools",
+  },
+};
